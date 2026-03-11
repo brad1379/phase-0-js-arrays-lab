e2fafb00039fa5a622b3b344382cff682c8b8ca7
Brad Arnold <brad1379@gmail.com>
2026-03-11 15:10:59 -0400

    Initial commit

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,7 +1,27 @@
 // Write your code here
+const products = ["Laptop", "Phone", "Headphones", "Monitor"]
+
+function logFirstProduct(){
+  console.log(products[0])
+} 
+function addProduct(name) {
+  products.push(name)
+}
+
+function updateProductName(number, name) {
+  products[number] = name
+}
+
+function removeLastProduct() {
+  products.pop()
+}
 
 
-
+// removeLastProduct(products)
+// addProduct(products, "Celery")
+// console.log(products)
+// updateProductName(products, 3, "Keyboard")
+// console.log(products)
 // Export the necessary parts for testing
 module.exports = {
   logFirstProduct: typeof logFirstProduct !== 'undefined' ? logFirstProduct : undefined,
